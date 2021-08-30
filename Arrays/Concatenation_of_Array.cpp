f5ae62b47ac46d99a5c47e710f628bd1977e32ee
Tom Michael <74369688+codeslayer73@users.noreply.github.com>
2021-08-30 18:53:33 +0530

    Update Concatenation_of_Array.cpp

--- Arrays/Concatenation_of_Array.cpp
+++ Arrays/Concatenation_of_Array.cpp
@@ -13,3 +13,36 @@ public:
         return ans;
     }
 };
+
+//optimal in run time code
+class Solution
+{
+public:
+    vector<int> getConcatenation(vector<int> &nums)
+    {
+        vector<int> res(nums.size() * 2);
+        int sz = nums.size();
+        for (int i = 0; i < res.size(); i++)
+        {
+            res[i] = nums[i % sz];
+        }
+        return res;
+    }
+};
+
+//optimal in space complexity
+class Solution
+{
+public:
+    vector<int> getConcatenation(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> ans(2 * n, 0);
+        for (int i = 0; i < n; i++)
+        {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+        return ans;
+    }
+};
